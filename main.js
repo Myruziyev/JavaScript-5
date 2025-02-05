15e722d0007a3c4c17bf85d3468df0111f69adc3
Myruziyev <myruziyev@gmail.com>
2025-02-05 16:11:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let aName = prompt("Ismingizni kiriting");
+let id = prompt("Space idingizni kiriting");
+let password = prompt("Space passwordingizni kiriting");
+let answerObj = { name: aName, id: id, password: password };
+console.log(answerObj);
